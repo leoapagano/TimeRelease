69ad882c23f8813f87bd3a666b77339741343dfc
Leo Pagano <leo@leoapagano.com>
2025-07-29 22:24:37 -0400

    Add optional CPU benchmarking to improve TimeRelease accuracy

--- encrypt_secret.py
+++ encrypt_secret.py
@@ -2,6 +2,7 @@ import base64
 import hashlib
 import json
 import random
+import time
 from Crypto.Cipher import AES
 from Crypto.Util.Padding import pad
 from Crypto.Random import get_random_bytes
@@ -11,6 +12,46 @@ from sympy import nextprime
 def byte_str_to_base64(byte_string):
 	"""Converts a byte string to its base64 (non-byte string) representation."""
 	return base64.b64encode(byte_string).decode('ASCII')
+
+
+def run_single_benchmark(iterations, logging=True):
+	"""Runs a benchmark to determine how long it takes for the CPU to run a fixed number of iterations (returned as a float)."""
+	# Setup dummy puzzle parameters
+	if logging: print("Preparing time lock...")
+	p = nextprime(random.getrandbits(512))
+	q = nextprime(random.getrandbits(512))
+	modulus = p * q
+
+	# Execute & time it
+	if logging: print("Running benchmark...")
+	start_time = time.time()
+	r = random.randint(2, modulus - 1)
+	for _ in range(iterations):
+		r = pow(r, 2, modulus)
+	end_time = time.time()
+
+	return end_time - start_time
+
+
+def run_benchmark(benches=10, logging=True):
+	"""Determines how many iterations the user's CPU is capable of processing per second (as an integer)."""
+	# Repeat single benchmarks, doubling iterations until the time per benchmark is less than 1 second
+	iterations = 1
+	execution_time = 0
+	while execution_time < 1:
+		iterations *= 2
+		if logging: print(f"Running benchmark @ iterations={iterations}")
+		execution_time = run_single_benchmark(iterations, logging=logging)
+	
+	# Run benches (default: 10) benchmarks with this number of iterations
+	results = []
+	for i in range(benches):
+		if logging: print(f"Running standard benchmark {i+1} / {benches}")
+		results.append(run_single_benchmark(iterations, logging=logging))
+
+	# Determine average iterations/sec and return
+	avg = sum(results) / benches
+	return int(iterations // avg)
 
 
 def encrypt_secret(secret, iterations, logging=True):
@@ -55,15 +96,35 @@ def encrypt_secret(secret, iterations, logging=True):
 
 
 if __name__ == "__main__":
-	# Get inputs
-	# we choose 2^18 because it's approximately one second on my machine
-	# later we will implement benchmarking
+	# Benchmark
+	print("Would you like to benchmark your CPU to provide more accurate unlock times? (Note that this may be thwarted by using a CPU with better single-core performance)")
+	while True:
+		response = input("[y/n] ").strip().lower()
+		if response in ("y", "yes"):
+			bench = True
+			break
+		elif response in ("n", "no"):
+			bench = False
+			break
+		else:
+			print("Please enter 'y' or 'n'.")
+
+	if bench:
+		# Run benchmark
+		print("TimeRelease is benchmarking your CPU. This may take up to a minute.")
+		ips = run_benchmark(logging=False)
+		print("Approximately how long would you like the unlock process to take on your CPU (in seconds)?")
+		iters = int(float(input("[float] ")) * ips)
+	else:
+		# Ask for raw iterations
+		print("How many iterations? Note that modern CPUs can handle upwards of 100,000 iterations per second.")
+		iters = int(input("[int] ")) * 2**18
+
+	# Get secret
 	print("Enter your secret:")
 	secret = input(">>> ").encode()
-	print("How many iterations? (Times 2^18)")
-	iters = int(input(">>> ")) * 2**18
 
-	# Get encrypted JSON package
+	# Encrypt secret & get JSON package
 	enc_package = encrypt_secret(secret, iters)
 
 	# Write it to timerelease.json
