1d159b7bb8918f6def92f957b37e744582e8e386
Leo Pagano <leo@leoapagano.com>
2026-03-06 08:10:35 +0000

    Fix type error where byte_str_to_base64 expected a byte string but got a bytearray

--- TimeRelease/b64utils.py
+++ TimeRelease/b64utils.py
@@ -6,6 +6,6 @@ def base64_to_byte_str(base64_string: str) -> bytes:
 	return base64.b64decode(base64_string.encode("ASCII"))
 
 
-def byte_str_to_base64(byte_string: bytes) -> str:
+def byte_str_to_base64(byte_string: bytes | bytearray | memoryview) -> str:
 	"""Converts a byte string to its base64 (non-byte string) representation."""
 	return base64.b64encode(byte_string).decode("ASCII")
